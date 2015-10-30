db2bfff0884e65519fe420605c3a6a9e578c71ff
Robert Dickenson <robert.dickenson@gmail.comen>
2015-10-30 19:32:24 +1100

    commented out duplicate define in the builds i'm currently testing, STM32F4/PX4 and VC++/SILSIM

--- FreeRTOS/include/FreeRTOS.h
+++ FreeRTOS/include/FreeRTOS.h
@@ -740,7 +740,7 @@ V8 if desired. */
 	#define xTimerHandle TimerHandle_t
 	#define xCoRoutineHandle CoRoutineHandle_t
 	#define pdTASK_HOOK_CODE TaskHookFunction_t
-	#define portTICK_RATE_MS portTICK_PERIOD_MS
+//	#define portTICK_RATE_MS portTICK_PERIOD_MS
 
 	/* Backward compatibility within the scheduler code only - these definitions
 	are not really required but are included for completeness. */
