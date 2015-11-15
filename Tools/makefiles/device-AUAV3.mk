949473688def2bc8f9c42b227f5940bba330ded9
Robert Dickenson <robert.dickenson@gmail.comen>
2015-11-16 01:46:41 +1100

    update the script based build system

--- Tools/makefiles/device-AUAV3.mk
+++ Tools/makefiles/device-AUAV3.mk
@@ -6,5 +6,5 @@ TARGET_TYPE := hex
 CPU := 33EP512MU810
 
 modules += libUDB libVectorMatrix libFlashFS Microchip
-incpath += Microchip Microchip/Include libVectorMatrix
+incpath += Microchip Microchip/Include libFlashFS libVectorMatrix
 #cfgpath := Config
