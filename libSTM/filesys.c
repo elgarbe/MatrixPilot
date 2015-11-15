44986a6be9183a95b1b500fe540667a4037df623
Robert Dickenson <robert.dickenson@gmail.comen>
2015-11-16 02:20:45 +1100

    standardise header

--- libSTM/filesys.c
+++ libSTM/filesys.c
@@ -1,10 +1,23 @@
+// This file is part of MatrixPilot.
 //
-//  filesys.c
-//  MatrixPilot
+//    http://code.google.com/p/gentlenav/
 //
-//  Created by Robert Dickenson on 17/6/2014.
-//  Copyright (c) 2014 MatrixPilot. All rights reserved.
+// Copyright 2009-2011 MatrixPilot Team
+// See the AUTHORS.TXT file for a list of authors of MatrixPilot.
 //
+// MatrixPilot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MatrixPilot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MatrixPilot.  If not, see <http://www.gnu.org/licenses/>.
+
 
 //#include <_ansi.h>
 //#include <sys/types.h>
